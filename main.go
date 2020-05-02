09b015f847b961ee0f2c9591c667ce45c81ddc0c
unknown <ugras_ergun@yahoo.com>
2020-05-02 21:38:25 +0200

    added semaphore

--- main.go
+++ main.go
@@ -15,17 +15,21 @@ func main() {
 
 	fmt.Println(*threadCount)
 
+	semaphore := make(chan bool, *threadCount)
+
 	urls := flag.Args()
 
 	var wg sync.WaitGroup
 
 	for _, url := range urls {
 		wg.Add(1)
+		semaphore <- true
 		go func(url string) {
+			defer wg.Done()
+			defer func() { <-semaphore }()
 			uri, md5 := getURIMD5(url)
 			fmt.Println(uri, md5)
 			time.Sleep(2 * time.Second)
-			defer wg.Done()
 		}(url)
 	}
 
